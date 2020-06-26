45ca69a04e7a780faa915918420dc2b511917a18
mgoplerud <mgoplerud@pitt.edu>
2020-06-26 13:08:35 -0400

    MAVB Tests

--- R/MAVB_functions.R
+++ R/MAVB_functions.R
@@ -4,43 +4,49 @@
 #' dissertation for details. This function uses a naive loop so is slower than
 #' necessary. This will be updated shortly.
 #' 
-#' @param model Model fit using vglmer
+#' @param object Model fit using vglmer
 #' @param samples Samples to draw from MAVB distribution.
 #' @param var_px Default (Inf); variance of working prior. Higher is more
 #'   diffuse and thus likely better.
 #' @import CholWishart
 #' @importFrom mvtnorm rmvnorm
 #' @export
-MAVB <- function(model, samples, var_px = Inf){
+MAVB <- function(object, samples, var_px = Inf){
   
-  # if (!inherits(model, 'vglmer')){
-  #   stop('Must provide model from vglmer')
-  # }
+  if (!inherits(object, 'vglmer')){
+    stop('Must provide object from vglmer')
+  }
+  if (object$family != 'binomial'){stop('MAVB only implemented for binomial at present.')}
   
-  M_prime <- model$MAVB_parameters$M_prime
-  M_prime_one <- model$MAVB_parameters$M_prime_one
-  M_mu_to_beta <- model$MAVB_parameters$M_mu_to_beta
-  d_j <- model$MAVB_parameters$d_j
-  g_j <- model$MAVB_parameters$g_j
-  outer_alpha_RE_positions <- model$MAVB_parameters$outer_alpha_RE_positions
-  factorization_method <- model$factorization_method
+  M_prime <- object$MAVB_parameters$M_prime
+  M_prime_one <- object$MAVB_parameters$M_prime_one
+  M_mu_to_beta <- object$MAVB_parameters$M_mu_to_beta
+  d_j <- object$MAVB_parameters$d_j
+  g_j <- object$MAVB_parameters$g_j
+  outer_alpha_RE_positions <- object$MAVB_parameters$outer_alpha_RE_positions
+  factorization_method <- object$control$factorization_method
   
-  if (model$factorization_method == 'weak'){
-    decomp_joint <- model$joint
-    joint_mean <- rbind(model$beta$mean, model$alpha$mean)
+  if (factorization_method == 'weak'){
+    decomp_joint <- object$joint
+    joint_mean <- rbind(object$beta$mean, object$alpha$mean)
     p.XZ <- ncol(decomp_joint)
-    p.X <- nrow(model$beta$mean)
+    p.X <- nrow(object$beta$mean)
   }else{
-    decomp_varA <- model$alpha$decomp_var
-    decomp_varB <- model$beta$decomp_var
+    decomp_varA <- object$alpha$decomp_var
+    decomp_varB <- object$beta$decomp_var
     p.XZ <- ncol(decomp_varA) + ncol(decomp_varB)
-    p.X <- nrow(model$beta$mean)
+    p.X <- nrow(object$beta$mean)
   }
   
   MAVB_sims <- matrix(NA, nrow = samples, ncol = p.XZ)
   regen_store <- MAVB_diff <- matrix(NA, nrow = samples, ncol = sum(d_j))
   
   n_MAVB <- samples
+  
+  alpha_mean <- object$alpha$mean
+  beta_mean <- object$beta$mean
+  sigma_df <- object$sigma$df
+  sigma_cov <- object$sigma$cov
   
   for (it in 1:n_MAVB){
     if (it %% 1000 == 0){cat('.')}
@@ -50,10 +56,10 @@ MAVB <- function(model, samples, var_px = Inf){
       sim_beta <- sim_joint[1:p.X,,drop=F]
       sim_a <- sim_joint[-1:-p.X,,drop=F]
     }else{
-      sim_a <- model$alpha$mean + t(decomp_varA) %*% rnorm(ncol(decomp_varA))
-      sim_beta <- model$beta$mean + t(decomp_varB) %*% rnorm(ncol(decomp_varB))
+      sim_a <- alpha_mean + t(decomp_varA) %*% rnorm(ncol(decomp_varA))
+      sim_beta <- beta_mean + t(decomp_varB) %*% rnorm(ncol(decomp_varB))
     }
-    sim_sigma <- mapply(model$sigma$df, model$sigma$cov, SIMPLIFY = FALSE, FUN=function(i,j){rInvWishart(n = 1, df = i, Sigma = j)[,,1]})
+    sim_sigma <- mapply(sigma_df, sigma_cov, SIMPLIFY = FALSE, FUN=function(i,j){rInvWishart(n = 1, df = i, Sigma = j)[,,1]})
     #Working Prior
     if (var_px == Inf){
       sim_px <- rep(0, sum(d_j))
@@ -93,9 +99,9 @@ MAVB <- function(model, samples, var_px = Inf){
 #' @inheritParams MAVB
 #' @inheritParams vglmer_predict
 #' @export
-MAVB_linpred <- function(model, newdata, samples, var_px = Inf, summary = TRUE){
-  pxSamples <- MAVB(model = model, samples = samples, var_px = var_px)
-  lp <- predict.vglmer(model, newdata = newdata, samples = pxSamples, summary = summary)
+predict_MAVB <- function(object, newdata, samples, var_px = Inf, summary = TRUE){
+  pxSamples <- MAVB(object = object, samples = samples, var_px = var_px)
+  lp <- predict.vglmer(object, newdata = newdata, samples = pxSamples, summary = summary)
   return(lp)
 }
 
--- tests/testthat/test-MAVB.R
+++ tests/testthat/test-MAVB.R
@@ -1,2 +1,63 @@
 context('Test MAVB')
 
+test_that('Short test of MAVB running for CRAN', {
+  
+  N <- 1000
+  G <- 10
+  x <- rnorm(N)
+  g <- sample(1:G, N, replace = T) 
+  g2 <- sample(1:G, N, replace = T)
+  alpha <- rnorm(G)
+  alpha2 <- rnorm(G)
+  
+  y <- rbinom(n = N, size = 1, prob = plogis(-1 + x + alpha[g] + alpha2[g]))
+  
+  est_glmer <- suppressMessages(suppressWarnings(lme4::glmer(y ~ x + (1 + x | g) + (1 | g2), family = binomial)))
+  
+  example_vglmer <- vglmer(formula = y ~ x + (1 + x | g) + (1 | g2), data = NULL, family = 'binomial',
+                           control = vglmer_control(factorization_method = 'strong'))
+  
+  mavb_samples <- MAVB(object = example_vglmer, samples = 10)
+  
+  
+})
+
+test_that('Longer Test of MAVB', {
+  
+  skip_on_cran()
+  
+  N <- 1000
+  G <- 10
+  x <- rnorm(N)
+  g <- sample(1:G, N, replace = T) 
+  g2 <- sample(1:G, N, replace = T)
+  alpha <- rnorm(G)
+  alpha2 <- rnorm(G)
+  
+  y <- rbinom(n = N, size = 1, prob = plogis(-1 + x + alpha[g] + alpha2[g]))
+  
+  est_glmer <- suppressMessages(suppressWarnings(lme4::glmer(y ~ x + (1 + x | g) + (1 | g2), family = binomial)))
+  glmer_var <- format_glmer(est_glmer)$var
+  example_vglmer <- vglmer(formula = y ~ x + (1 + x | g) + (1 | g2), data = NULL, family = 'binomial',
+                           control = vglmer_control(factorization_method = 'strong'))
+  example_vglmer_weak <- vglmer(formula = y ~ x + (1 + x | g) + (1 | g2), data = NULL, family = 'binomial',
+                           control = vglmer_control(factorization_method = 'weak'))
+  
+  mavb_samples <- MAVB(object = example_vglmer, samples = 2000)
+  
+  mavb_colvar <- colVar(mavb_samples)
+  mavb_colmeans <- colMeans(mavb_samples)
+  
+  vi_means <- as.vector(rbind(example_vglmer$beta$mean, example_vglmer$alpha$mean))  
+  vi_var <- as.vector(c(diag(vcov(example_vglmer)), example_vglmer$alpha$dia.var))  
+  vi_var_weak <- as.vector(c(diag(vcov(example_vglmer_weak)), example_vglmer_weak$alpha$dia.var))  
+  
+  #Mean should be closer
+  expect_lte(max(abs(vi_means - mavb_colmeans)), .1)
+  #Variances should be larger
+  expect_gte(min(mavb_colvar - vi_var), 0)
+  # and closer to weak (Scheme III)
+  expect_gte(median(abs(vi_var_weak - vi_var)), median(abs(vi_var_weak - mavb_colvar)))
+  
+})
+
